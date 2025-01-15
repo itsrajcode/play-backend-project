00d97f5a1fd145dde8f0517f82dea4530bbfd703
Rajan Prajapati <rajanprajapati0526@gmail.com>
2025-01-15 16:03:36 +0530

    Feat: adding tokens and aggregassion

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -7,10 +9,12 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    watchHistory: [{
+    watchHistory: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Video"
-    }],
+        ref: "Video",
+      },
+    ],
     username: {
       type: String,
       required: true,
@@ -40,7 +44,6 @@ const userSchema = new mongoose.Schema(
     },
     coverImage: {
       type: String,
-
     },
     refreshToken: {
       type: String,
@@ -49,5 +52,42 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt
+    .hash(this.password, salt)
+    .catch((error) => next(error));
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.refreshAccessToken = function () {
+    return jwt.sign(
+        {
+          _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+          expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+      );
+};
 export const User = mongoose.model("User", userSchema);
-
