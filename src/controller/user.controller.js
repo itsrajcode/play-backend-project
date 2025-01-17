10682e7a7b375d1e16cdcee3b853bcb864723a55
Rajan Prajapati <rajanprajapati0526@gmail.com>
2025-01-17 10:22:33 +0530

    Fix: fix controller and route

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,10 +1,29 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError.js";
 
+const registerUser = asyncHandler(async (req, res, next) => {
+  const { username, email, fullname, password, avatar } = req.body;
 
-const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "User registered successfully"
-    })
-})
+  if ((!username || !email || !fullname || !password || !avatar)) {
+    return next(new ApiError(400, "Please provide all fields"));
+  }
 
-export { registerUser }
+  const userExists =
+    (await User.findOne({ email })) || (await User.findOne({ username }));
+
+  if (userExists) {
+    return next(new ApiError(400, "User already exists"));
+  }
+
+  const user = await User.create({ username, email, fullname, password, avatar });
+  user.password = undefined;
+
+  res.status(201).json({
+    success: true,
+    message: "User created successfully",
+    data: user,
+  });
+});
+
+export { registerUser };
