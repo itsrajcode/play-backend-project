4f28785e0f2a74153369eefbe503ededbf28c126
Rajan Prajapati <rajanprajapati0526@gmail.com>
2025-02-11 19:01:55 +0530

    Feat: get twwets endpoint

--- src/controller/tweet.controller.js
+++ src/controller/tweet.controller.js
@@ -20,7 +20,18 @@ const createTweet = asyncHandler(async (req, res, next) => {
   return res.status(201).json(new ApiResponse(201, "Tweet created", newtweet));
 });
 
+const getTweets = asyncHandler(async (req, res, next) => {
+  const userId = req.params;
+  if (!isValidObjectId(userId)) {
+    return next(new ApiError(400, "Invalid user id"));
+  }
+  const tweets = await Tweet.find({ owner: userId }).sort({ createdAt: -1 });
+  if (!tweets || tweets.length === 0) {
+    return next(new ApiError(404, "No tweets found"));
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Tweets fetched successfully", tweets));
+});
 
-
-
-export { createTweet };
+export { createTweet, getTweets };
